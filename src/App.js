4677bec7105c36e1bb74c50c0d5fe83ba728d652
byio <ben@benyang.io>
2018-02-21 10:48:29 +0800

    set up reusable components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import { Header } from './components/common';
+
 class App extends Component {
 
   render () {
     return (
       <View>
+        <Header headerText={"Authenticated"}/>
         <Text>
           src/App.js
         </Text>
